a2b71ccea5f5b61d1af087e47ea64bb4d62df3d1
Beyza Uğur <beyzaugur000@gmail.com>
2026-05-14 18:22:28 +0300

    Settings.Designer.cs

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace BÜT_Mehmed_Akif_Aydemir.Properties
+namespace HotelReservationSystem.Properties
 {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
